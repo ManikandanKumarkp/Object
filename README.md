258ddba556521e6c7d8b31fd1ff984bc79433a7e
ManikandanKumarkp <manikandankumarkp@gmail.com>
2022-04-04 18:35:23 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-yt1upx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-yt1upx)
---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular ' + VERSION.major;
+
+  let person = { fname:"mani ",lname :"kandan ",
+  fullname : function () {
+    return this.person.fname + " " + this.person.lname; }
+ };
+}
